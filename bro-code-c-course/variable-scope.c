d42cceae61f36835ed1fc5dc30ac09f0e35a700b
Dylan Jiao <dylanjiao2001@gmail.com>
2026-01-19 11:31:49 -0500

    feat: add variable scopes practice

---
+++ bro-code-c-course/variable-scope.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+// int result = 0; // Global scope (hard to debug)
+
+int add(int x, int y){
+    int result = x + y;
+    return result;
+}
+
+int substract(int x, int y){
+    int result = x - y;
+    return result;
+}
+
+int main(){
+
+    // variable scope = Refers to where a variable is recognized and accessible.
+    //                  Variables can share the same name if
+    //                  they're in different scopes {}
+
+    int x = 5;
+    int y = 6;
+
+    int result = substract(x, y);
+
+    printf("%d", result);
+
+    return 0;
+}
